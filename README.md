f2df4e8105ad27baf5020ecd31bad31730eac214
Vresod <lukebinkofsky@gmail.com>
2020-10-26 15:23:25 -0400

    feat: add c!arecringe for plural words and updated c!help

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 # IsCringe-Bot
 
 says "thing is cringe". thats it. thanks.
+
+stolen from something i did for kraken-bot
--- main.py
+++ main.py
@@ -9,12 +9,13 @@ with open("tokenfile","r") as tokenfile:
 client = commands.Bot(command_prefix="c!")
 client.remove_command("help")
 
-def cringeify(text,length):
+def cringeify(text,length,plurality):
 	if len(text) > length:
 		rtext = f"{text[:length - 3]}..."
 	else:
 		rtext = text
-	return f"{rtext} is cringe"
+	isword = "is" if not plurality else "are"
+	return f"{rtext} {isword} cringe"
 
 @client.event
 async def on_ready():
@@ -34,7 +35,7 @@ async def iscringe(ctx,*text):
 			if message == ctx.message:
 				continue
 			ctext = message.content
-	await ctx.send(f"{cringeify(ctext,1970)}")
+	await ctx.send(f"{cringeify(ctext,1970,False)}")
 
 @client.command()
 async def saidcringe(ctx,*text):
@@ -46,11 +47,22 @@ async def saidcringe(ctx,*text):
 				continue
 			ctext = message.content
 	ctext = f"\"{ctext}\""
-	await ctx.send(f"saying {cringeify(ctext,1978)}")
+	await ctx.send(f"saying {cringeify(ctext,1978,False)}")
+
+@client.command()
+async def arecringe(ctx,*text):
+	if text != ():
+		ctext = " ".join(text)
+	else:
+		async for message in ctx.channel.history(limit=2):
+			if message == ctx.message:
+				continue
+			ctext = message.content
+	await ctx.send(f"{cringeify(ctext,1971,True)}")
 
 @client.command()
 async def help(ctx):
-	await ctx.send("use c!iscringe to mark something as cringe")
+	await ctx.send("theres:\n1. c!saidcringe for saying that saying something is cringe\n2. c!iscringe for saying something is cringe\n3. c!arecringe for saying somethings are cringe")
 
 @client.command()
 async def naenae(ctx):
